5040d2c175e742605258eccdcd0292946a9aff3d
whalemare <nanostet@gmail.com>
2020-02-14 14:06:19 +0700

    Add test for get versionNumber

--- src/model/platform/IOSPlatform.ts
+++ src/model/platform/IOSPlatform.ts
@@ -1,7 +1,7 @@
 import { Version } from '../../cli/utils'
 import { error } from '../../ui/error'
 import jetpack = require('fs-jetpack')
-import shell from 'shelljs'
+import shell, { ShellString } from 'shelljs'
 
 export class IOSPlatform {
   projectDirectory: string
@@ -27,11 +27,25 @@ export class IOSPlatform {
    * Be sure, that you enable `agvtool` for your project https://dzone.com/articles/agvtool-automating-ios-build-and-version-numbers
    */
   async getBuildNumber(): Promise<number> {
-    const result = shell.cd(this.iosDirectory).exec('xcrun agvtool what-version')
+    const result = this.exec('xcrun agvtool what-version')
+    const buildNumber = Number.parseInt(result.stdout.match(/\d+/g).join())
+    return buildNumber
+  }
+
+  async getVersionName(): Promise<string> {
+    const result = this.exec('xcrun agvtool what-marketing-version')
+    const phrase = 'Found CFBundleShortVersionString of "'
+    const startIndex = result.indexOf(phrase)
+    const endIndex = result.indexOf('"', startIndex + phrase.length + 1)
+    const version = result.substring(startIndex + phrase.length, endIndex)
+    return version
+  }
+
+  private exec(command: string): ShellString {
+    const result = shell.cd(this.iosDirectory).exec(command)
     if (result.code != 0) {
       throw result.stderr
     }
-    const buildNumber = Number.parseInt(result.stdout.match(/\d+/g).join())
-    return buildNumber
+    return result
   }
 }
--- test/platform/ios.test.ts
+++ test/platform/ios.test.ts
@@ -1,4 +1,4 @@
-import { IOSPlatform } from '../../src'
+import { IOSPlatform, ios } from '../../src'
 import jetpack = require('fs-jetpack')
 import chai from 'chai'
 
@@ -12,5 +12,15 @@ describe('ios platform actions', () => {
   it('should return build number of ios project', async () => {
     const result = await iosPlatform.getBuildNumber()
     chai.assert.isAbove(result, 0)
+  })
+
+  it('should return versionName', async () => {
+    const result = await iosPlatform.getVersionName()
+    const parts = result.split('.')
+    chai.assert.include(result, '.', 'versionName should include dots')
+
+    parts.map(part => {
+      chai.assert.isNumber(Number.parseInt(part), 'ios version number should include only numbers')
+    })
   })
 })
