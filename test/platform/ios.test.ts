666022da25d5d8fa3c018940ef540d769889cf43
whalemare <nanostet@gmail.com>
2020-02-14 14:48:38 +0700

    Add running macos specific test only on macos

--- test/platform/ios.test.ts
+++ test/platform/ios.test.ts
@@ -8,6 +8,12 @@ const iosProjectPath = jetpack
   .path()
 const iosPlatform = new IOSPlatform(null, iosProjectPath)
 
+function onlyMacOs<T>(block: () => T): T {
+  if (process.platform == 'darwin') {
+    return block()
+  }
+}
+
 function assertVersionName(version: string) {
   version.split('.').forEach(part => {
     chai.assert.isNumber(Number.parseInt(part), 'ios version number should include only numbers')
@@ -16,26 +22,34 @@ function assertVersionName(version: string) {
 
 describe('ios platform actions', () => {
   it('should return build number of ios project', async () => {
-    const result = await iosPlatform.getBuildNumber()
-    chai.assert.isAbove(result, 0)
+    await onlyMacOs(async () => {
+      const result = await iosPlatform.getBuildNumber()
+      chai.assert.isAbove(result, 0)
+    })
   })
 
   it('should return versionName', async () => {
-    const result = await iosPlatform.getVersionName()
-    assertVersionName(result)
+    await onlyMacOs(async () => {
+      const result = await iosPlatform.getVersionName()
+      assertVersionName(result)
+    })
   })
 
   it('should setVersion', async () => {
-    const versionForUpdate = '3.4.5'
-    const [oldVersion, newVersion] = await iosPlatform.setVersionName(versionForUpdate)
-    chai.assert.equal(versionForUpdate, newVersion)
-    assertVersionName(oldVersion)
+    await onlyMacOs(async () => {
+      const versionForUpdate = '3.4.5'
+      const [oldVersion, newVersion] = await iosPlatform.setVersionName(versionForUpdate)
+      chai.assert.equal(versionForUpdate, newVersion)
+      assertVersionName(oldVersion)
+    })
   })
 
   it('should increment build number', async () => {
-    const [oldBuildNumber, newBuildNumber] = await iosPlatform.incrementBuildNumber()
-    chai.assert.isNumber(oldBuildNumber)
-    chai.assert.isNumber(newBuildNumber)
-    chai.assert.equal(newBuildNumber, oldBuildNumber + 1)
+    await onlyMacOs(async () => {
+      const [oldBuildNumber, newBuildNumber] = await iosPlatform.incrementBuildNumber()
+      chai.assert.isNumber(oldBuildNumber)
+      chai.assert.isNumber(newBuildNumber)
+      chai.assert.equal(newBuildNumber, oldBuildNumber + 1)
+    })
   })
 })
