997d5304653ef82a6f8518f2b8739ba239f25638
whalemare <nanostet@gmail.com>
2020-02-17 11:59:06 +0700

    Fix path to tests

--- test/integration/publish.test.ts
+++ test/integration/publish.test.ts
@@ -5,7 +5,7 @@ describe(`integration test of publish`, function() {
   this.timeout(10000)
 
   it(`should show help for program`, done => {
-    shell.exec('./build/src/cli/index.js -h', code => {
+    shell.exec('./build/cli/index.js -h', code => {
       if (code != 0) {
         done(code)
       } else {
@@ -13,4 +13,15 @@ describe(`integration test of publish`, function() {
       }
     })
   })
+
+  // it(`should apply env file`, done => {
+  //   const envFile = __dirname + '/../assets/.test.env'
+  //   shell.exec(`./build/cli/index.js android --track beta -e ${envFile}`, code => {
+  //     if (code != 0) {
+  //       done(code)
+  //     } else {
+  //       done()
+  //     }
+  //   })
+  // })
 })
