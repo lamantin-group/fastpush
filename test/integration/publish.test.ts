ad4b622fd81235b6af6a9b0ff6a1f1f8f2c300a2
whalemare <nanostet@gmail.com>
2020-02-18 10:10:06 +0700

    Fix script integration

--- test/integration/publish.test.ts
+++ test/integration/publish.test.ts
@@ -5,7 +5,7 @@ describe(`integration test of publish`, function() {
   this.timeout(10000)
 
   it(`should show help for program`, done => {
-    shell.exec('./build/cli/index.js -h', code => {
+    shell.exec('yarn start:help', code => {
       if (code != 0) {
         done(code)
       } else {
