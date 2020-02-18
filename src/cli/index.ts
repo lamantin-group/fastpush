41a491045109142aaa5b597e17ab18f3e4dab30c
whalemare <nanostet@gmail.com>
2020-02-18 14:18:15 +0700

    test/1.3.21-41

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,9 +3,15 @@
 import { fastpush } from './fastpush'
 import { publish } from './publish'
 import { ui } from '../ui'
+import { git } from '../utils'
 
-const parsedOptions = fastpush(process.argv)
-publish(parsedOptions).catch(e => {
-  ui.error('Unhandler error: ' + e)
-  ui.error('Open issue: https://github.com/lamantin-group/fastpush/issues/new')
-})
+// const parsedOptions = fastpush(process.argv)
+// publish(parsedOptions).catch(e => {
+//   ui.error('Unhandler error: ' + e)
+//   ui.error('Open issue: https://github.com/lamantin-group/fastpush/issues/new')
+// })
+
+const tag = `test/1.3.21-41`
+git.commit(`${tag}`)
+git.tag(tag)
+git.push()
