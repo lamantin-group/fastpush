145e00f886923ffcd9aabec7446bf7f242b097f0
Anton Vlasov <nanostet@gmail.com>
2021-06-30 23:08:44 +0700

    Add docs

--- src/fastlane/Lane.ts
+++ src/fastlane/Lane.ts
@@ -5,5 +5,12 @@ export type Lane = {
 
 export type Argument = {
   name: string
+  /**
+   * Support: string, number, boolean, nested Argument[], nil and ENV
+   *
+   * Use "nil" when you need set value to empty
+   *
+   * Use `ENV["some-fastlane-env-variable"]` for access environment variable in runtime
+   */
   value: string | number | boolean | Argument[] | 'nil'
 }
